2e4e9ca017b5adae7c2edf89e8f841d9cfe93796
Thakuriprince <princethakuri11111@gmail.com>
2020-06-24 18:07:52 +0545

    'update'

--- 2.py
+++ 2.py
@@ -1,9 +1,9 @@
-def string(str):
+def string_length(str):
     if len(str)<2:
         return'Empty String'
     else:
         return str[0:2] + str[-2:]
 
-print(string('Python'))
-print(string('Py'))
-print(string('w'))
+print(string_length('Python'))
+print(string_length('Py'))
+print(string_length('w'))
---
+++ 6.py
@@ -0,0 +1,13 @@
+def not_poor(str):
+    snot = str.find('not')
+    spoor = str.find('poor')
+
+
+    if spoor > snot and snot>0 and spoor>0:
+        str = str.replace(str[snot:(spoor+4)],'good')
+        return str
+    else:
+        return str
+
+print(not_poor('the lyrics is not that poor'))
+print(not_poor('the lyrics is poor'))
